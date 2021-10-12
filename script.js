8949d32ca3a55d6c81b446267bb43baa70cbfbd5
Henry <hli219@ucsc.edu>
2021-10-11 17:58:09 -0700

    files
    adding files

---
+++ script.js
@@ -0,0 +1,38 @@
+// mobile nav
+const hamburger = document.getElementById('hamburger');
+const navUL = document.getElementById('nav-ul');
+
+hamburger.addEventListener('click', () => {
+    navUL.classList.toggle('show');
+});
+
+const header = document.querySelector('.header')
+window.onscroll = function(){
+    var top = window.scrollY;
+    if (top >= 10){
+        header.classList.add('active')
+    }
+    else{
+        header.classList.remove('active')
+    }
+}
+
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+  var reveals = document.querySelectorAll('.reveal');
+
+  for(var i = 0; i < reveals.length; i++){
+
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if(revealtop < windowheight - revealpoint){
+      reveals[i].classList.add('active');
+    }
+    else{
+      reveals[i].classList.remove('active');
+    }
+  }
+}
